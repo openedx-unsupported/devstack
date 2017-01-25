a5f249dc371c23727a134c420aaa9129d7cb71e1
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-01-25 00:54:24 -0500

    Added provision step to create demo courses and users for edxapp
    ECOM-6634
    
    Fixes #35

--- provision.sh
+++ provision.sh
@@ -49,6 +49,9 @@ docker exec -t edx.devstack.edxapp  bash -c 'source /edx/app/edxapp/edxapp_env &
 docker exec -t edx.devstack.edxapp  bash -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user edx edx@example.com --superuser --staff'
 docker exec -t edx.devstack.edxapp  bash -c 'source /edx/app/edxapp/edxapp_env && echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username=\"edx\"); user.set_password(\"edx\"); user.save()" | python /edx/app/edxapp/edx-platform/manage.py lms shell  --settings=devstack_docker' &
 
+# Create demo course and users
+docker exec -t edx.devstack.edxapp  bash -c '/edx/app/edx_ansible/venvs/edx_ansible/bin/ansible-playbook /edx/app/edx_ansible/edx_ansible/playbooks/edx-east/demo.yml -v -c local -i "127.0.0.1," --extra-vars="COMMON_EDXAPP_SETTINGS=devstack_docker"' &
+
 # We must fake an associative array for Bash 3 users
 services=('credentials:18150' 'discovery:18381' 'ecommerce:18130' 'programs:18140')
 
