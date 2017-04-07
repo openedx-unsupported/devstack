3f5a9c9b3019c2809285e52ccdd201ee7cdb8c32
Joel Barciauskas <joel@edx.org>
2017-04-07 12:41:57 -0400

    Break apart provisioning into constituent services pieces
    Also make destroy.sh delete anonymous disks (use -v with docker-compose)

---
+++ provision-ecommerce.sh
@@ -0,0 +1,8 @@
+# Load database dumps for the largest databases to save time
+./load-db.sh ecommerce
+
+./provision-ida.sh ecommerce 18130
+
+# Configure ecommerce
+docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && python /edx/app/ecommerce/ecommerce/manage.py create_or_update_site --site-id=1 --site-domain=localhost:18130 --partner-code=edX --partner-name="Open edX" --lms-url-root=http://edx.devstack.lms:18000 --theme-scss-path=sass/themes/edx.scss --payment-processors=cybersource,paypal --client-id=ecommerce-key --client-secret=ecommerce-secret --from-email staff@example.com'
+docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && python /edx/app/ecommerce/ecommerce/manage.py oscar_populate_countries --initial-only'
---
+++ provision-ida.sh
@@ -0,0 +1,20 @@
+name=$1
+port=$2
+
+docker-compose up -d $name
+
+echo -e "${GREEN}Installing requirements for ${name}...${NC}"
+docker exec -t edx.devstack.${name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make requirements' -- "$name"
+
+echo -e "${GREEN}Running migrations for ${name}...${NC}"
+docker exec -t edx.devstack.${name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make migrate' -- "$name"
+
+echo -e "${GREEN}Creating super-user for ${name}...${NC}"
+docker exec -t edx.devstack.${name}  bash -c 'source /edx/app/$1/$1_env && echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None" | python /edx/app/$1/$1/manage.py shell' -- "$name"
+
+./provision-ida-user.sh $name $port
+
+# Compile static assets last since they are absolutely necessary for all services. This will allow developers to get
+# started if they do not care about static assets
+echo -e "${GREEN}Compiling static assets for ${name}...${NC}"
+docker exec -t edx.devstack.${name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make static' -- "$name"
