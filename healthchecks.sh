d52a231d2a7bf83275f10d893900c7c9e88c95aa
Joel Barciauskas <barciajo@gmail.com>
2017-04-21 11:09:38 -0400

    Add discovery healthcheck

--- healthchecks.sh
+++ healthchecks.sh
@@ -9,3 +9,6 @@ curl http://localhost:18010/heartbeat # Studio
 echo
 echo "Checking ecommerce health:"
 curl http://localhost:18130/health/ # Ecommerce
+echo
+echo "Checking discovery health:"
+curl http://localhost:18381/health/ # Discovery
