e98f4bfe68fba010d34f86da6970906b2bbdb4d9
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-06-09 01:12:43 -0400

    Created IDA users as superusers to avoid permissions issues
    We don't have a simple way of managing groups at the moment. This is the next best alternative as managing groups/permissions is a bit beyond the scope of the current task.
    
    LEARNER-1375

--- provision-ida-user.sh
+++ provision-ida-user.sh
@@ -4,5 +4,5 @@ name=$1
 port=$2
 
 echo -e "${GREEN}Creating service user and OAuth client for ${name}...${NC}"
-docker exec -t edx.devstack.lms  bash -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user $1_worker $1_worker@example.com --staff' -- "$name"
+docker exec -t edx.devstack.lms  bash -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user $1_worker $1_worker@example.com --staff --superuser' -- "$name"
 docker exec -t edx.devstack.lms  bash -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_oauth2_client "http://localhost:$2" "http://localhost:$2/complete/edx-oidc/" confidential --client_name $1 --client_id "$1-key" --client_secret "$1-secret" --trusted --logout_uri "http://localhost:$2/logout/" --username $1_worker' -- "$name" "$port"
