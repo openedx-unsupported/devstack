05be0427a5d9bafff13f496cb1ccb24dbac8ef54
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-06-14 08:50:32 -0400

    Removed stray pip install statement
    That shouldn't be there!
    
    LEARNER-1375

--- provision-ecommerce.sh
+++ provision-ecommerce.sh
@@ -6,4 +6,4 @@
 # Configure ecommerce
 docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && python /edx/app/ecommerce/ecommerce/manage.py create_or_update_site --site-id=1 --site-domain=localhost:18130 --partner-code=edX --partner-name="Open edX" --lms-url-root=http://edx.devstack.lms:18000 --client-side-payment-processor=cybersource --payment-processors=cybersource,paypal --client-id=ecommerce-key --client-secret=ecommerce-secret --from-email staff@example.com'
 docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && python /edx/app/ecommerce/ecommerce/manage.py oscar_populate_countries --initial-only'
-docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && pip install zeep && python /edx/app/ecommerce/ecommerce/manage.py create_demo_data --partner=edX'
+docker exec -t edx.devstack.ecommerce bash -c 'source /edx/app/ecommerce/ecommerce_env && python /edx/app/ecommerce/ecommerce/manage.py create_demo_data --partner=edX'
