6955d5b2b5690e3e5724d016cc710287516fb64e
Brian Beggs <bbeggs@edx.org>
2017-07-18 16:21:25 -0400

    Remove installing prereqs from lms devstack provisioning

--- provision-lms.sh
+++ provision-lms.sh
@@ -9,13 +9,8 @@ set -x
 # Bring LMS online
 docker-compose $DOCKER_COMPOSE_FILES up -d lms
 
-docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && NO_PYTHON_UNINSTALL=1 paver install_prereqs'
-
-#Installing prereqs crashes the process
-docker-compose restart lms
-
 # Run edxapp migrations first since they are needed for the service users and OAuth clients
-docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && NO_PREREQ_INSTALL=1 paver update_db --settings devstack_docker'
+docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_db --settings devstack_docker'
 
 # Create a superuser for edxapp
 docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user edx edx@example.com --superuser --staff'
