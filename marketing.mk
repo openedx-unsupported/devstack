94ca41abbd213600a2f835bdaa687906c16afda0
Robert Raposa <rraposa@edx.org>
2017-10-16 16:04:29 -0400

    Update marketing make tasks.
    - Add stop.
    - Fix message for down.
    - Add help-marketing.

--- marketing.mk
+++ marketing.mk
@@ -1,7 +1,14 @@
+help-marketing: ## Display this help message
+	@echo "Please use \`make <target>' where <target> is one of"
+	@perl -nle'print $& if m{^[\.a-zA-Z_-]+:.*?## .*$$}' $(MAKEFILE_LIST) | grep marketing | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m  %-25s\033[0m %s\n", $$1, $$2}'
+
 marketing-shell: ## Run a shell on the marketing site container
 	docker exec -it edx.devstack.marketing env TERM=$(TERM) bash
 
-down-marketing:   ## Bring up all services (including the marketing site) with host volumes
+stop-marketing:   ## Stop all services (including the marketing site) with host volumes
+	docker-compose -f docker-compose.yml -f docker-compose-host.yml -f docker-compose-marketing-site.yml -f docker-compose-marketing-site-host.yml stop
+
+down-marketing:   ## Bring down all services (including the marketing site) with host volumes
 	docker-compose -f docker-compose.yml -f docker-compose-host.yml -f docker-compose-marketing-site.yml -f docker-compose-marketing-site-host.yml down
 
 up-marketing:   ## Bring up all services (including the marketing site) with host volumes
