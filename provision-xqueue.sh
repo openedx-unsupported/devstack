9cf589025893fb6192194372f2b97893f62c6789
Kevin Falcone <kevin@edx.org>
2018-03-23 15:34:17 -0400

    Make sure xqueue(_consumer) has logs/shell
    Remove rabbit now that we no longer need it.
    This includes the provisioning

--- provision-xqueue.sh
+++ provision-xqueue.sh
@@ -2,8 +2,7 @@ set -e
 set -o pipefail
 set -x
 
-# Bring up RabbitMQ and XQueue
-docker-compose $DOCKER_COMPOSE_FILES up -d rabbitmq
+# Bring up XQueue, we don't need the consumer for provisioning
 docker-compose $DOCKER_COMPOSE_FILES up -d xqueue
 
 # This works in case you provision xqueue without having other services up
@@ -23,8 +22,3 @@ docker exec -i edx.devstack.mysql mysql -uroot mysql < provision-xqueue.sql
 docker-compose $DOCKER_COMPOSE_FILES exec xqueue bash -c 'source /edx/app/xqueue/venvs/xqueue/bin/activate && cd /edx/app/xqueue/xqueue && SERVICE_VARIANT=xqueue python manage.py migrate --settings=xqueue.devstack'
 # Add users that graders use to fetch data, there's one default user in Ansible which is part of our settings
 docker-compose $DOCKER_COMPOSE_FILES exec xqueue bash -c 'source /edx/app/xqueue/venvs/xqueue/bin/activate && cd /edx/app/xqueue/xqueue && SERVICE_VARIANT=xqueue python manage.py update_users --settings=xqueue.devstack'
-
-# Create a user that can be used by xqueue to create / manage queues
-docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl add_user edx edx'
-docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl set_permissions edx ".*" ".*" ".*"'
-docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl set_user_tags edx administrator'
