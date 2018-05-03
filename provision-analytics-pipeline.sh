4a7415897b7de650504679df2dc084e84f6f9788
Abdul Mannan <raoabdulmannan@gmail.com>
2018-05-03 15:45:50 +0500

    provison lms with migrations only

--- provision-analytics-pipeline.sh
+++ provision-analytics-pipeline.sh
@@ -32,9 +32,16 @@ if [[ ! -z "`docker exec -i edx.devstack.mysql mysql -uroot -se "SELECT SCHEMA_N
 then
   echo -e "${GREEN}LMS DB exists, skipping lms schema load.${NC}"
 else
+  # Provisioning lms db with migrations. Update above comment if these are approved.
   echo -e "${GREEN}LMS DB not found, load the schema.${NC}"
   docker exec -i edx.devstack.mysql mysql -uroot --max_allowed_packet=1073741824 mysql < provision.sql
   docker exec -i edx.devstack.mysql mysql -uroot --max_allowed_packet=1073741824 edxapp < edxapp.sql
+  docker-compose $DOCKER_COMPOSE_FILES up -d lms studio
+  docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && NO_PYTHON_UNINSTALL=1 paver install_prereqs'
+  #Installing prereqs crashes the process
+  docker-compose restart lms
+  # Run edxapp migrations first since they are needed for the service users and OAuth clients
+  docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_db --settings devstack_docker'
 fi
 
 echo -e "${GREEN}Creating databases and users...${NC}"
