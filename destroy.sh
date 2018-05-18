2da9f137759abacf93131b656914b9e248dc31d5
Abdul Mannan <raoabdulmannan@gmail.com>
2018-05-19 01:52:33 +0500

    Update old script

--- destroy.sh
+++ destroy.sh
@@ -5,12 +5,5 @@ set -e
 read -p "This will delete all data in your devstack. Would you like to proceed? [y/n] " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        set +e
-        docker-sync stop
-        docker-sync clean
-        set -e
-    fi
-    docker-compose down -v
+    docker-compose -f docker-compose.yml -f docker-compose-watchers.yml -f docker-compose-host.yml -f docker-compose-analytics-pipeline.yml down -v
 fi
