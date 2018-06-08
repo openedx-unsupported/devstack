86423196b6ad042c60e743c5f6903ceff4b20908
Troy Sankey <sankeytms@gmail.com>
2018-06-08 12:08:17 -0400

    Fix bug caused by provision-ida-user.sh refactor
    The provision-credentials.sh script was missed as part of a recent
    change to provision-ida-user.sh arguments.  Tests passed even with this
    error, perhaps due to return codes not bubbling up through
    docker-compose, or a missing set -e somewhere but I haven't debugged.

--- provision-credentials.sh
+++ provision-credentials.sh
@@ -20,7 +20,7 @@ docker exec -t edx.devstack.${name}  bash -c 'echo "from django.contrib.auth imp
 echo -e "${GREEN}Configuring site for ${name}...${NC}"
 docker exec -t edx.devstack.${name} bash -c './manage.py create_or_update_site --site-id=1 --site-domain=localhost:18150 --site-name="Open edX" --platform-name="Open edX" --company-name="Open edX" --lms-url-root=http://localhost:18000 --catalog-api-url=http://edx.devstack.discovery:18381/api/v1/ --tos-url=http://localhost:18000/tos --privacy-policy-url=http://localhost:18000/privacy --homepage-url=http://localhost:18000 --certificate-help-url=http://localhost:18000/faq --theme-name=openedx'
 
-./provision-ida-user.sh ${name} ${port}
+./provision-ida-user.sh ${name} ${name} ${port}
 
 # Compile static assets last since they are absolutely necessary for all services. This will allow developers to get
 # started if they do not care about static assets
