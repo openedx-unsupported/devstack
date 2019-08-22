306d4f0c2f364f14c6142730c004feaf2856d953
sanjaydatasciencedojo <sanjay@datasciencedojo.com>
2019-08-21 19:00:48 -0700

    Updated the travis script

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,14 +7,7 @@ if [[ $DEVSTACK == 'lms' ]]; then
     make dev.provision
     make dev.up
     sleep 60  # LMS needs like 60 seconds to come up
-    make healthchecks
-    make validate-lms-volume
-    # Disable e2e-tests until either:
-    # * tests are less flaky
-    # * We have a way to test the infrastructure for testing but ignore the test results.
-    # See PLAT-1712
-    # - make e2e-tests
-    make up-marketing-detached
+    make lms-static
 fi
 
 if [[ $DEVSTACK == 'analytics_pipeline' ]]; then
