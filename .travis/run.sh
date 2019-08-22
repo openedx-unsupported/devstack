7c599e4dd8b5928f18f88ddc2d58b987c40815e0
sanjaydatasciencedojo <sanjay@datasciencedojo.com>
2019-08-22 11:42:44 -0700

    Update file

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,15 +3,14 @@
 set -e
 set -x
 
-if [[ $DEVSTACK == 'lms' ]]; then
-    make dev.provision
-    wget https://raw.githubusercontent.com/sumbul03/edx-theme/master/lms.env.json
-    docker cp lms.env.json edx.devstack.lms:/edx/app/edxapp/lms.env.json
-    cat lms.env.json
-    make dev.up
-    sleep 60  # LMS needs like 60 seconds to come up
-    make lms-static
-fi
+make dev.provision
+wget https://raw.githubusercontent.com/sumbul03/edx-theme/master/lms.env.json
+docker cp lms.env.json edx.devstack.lms:/edx/app/edxapp/lms.env.json
+cat lms.env.json
+make dev.up
+sleep 60  # LMS needs like 60 seconds to come up
+make lms-static
+
 
 if [[ $DEVSTACK == 'analytics_pipeline' ]]; then
     make dev.provision.analytics_pipeline
