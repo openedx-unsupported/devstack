1392d8fcc95417de21804f2737b88476701856eb
Kyle McCormick <kmccormick@edx.org>
2019-12-13 17:45:52 -0500

    Add 'make dev.provision.<service>'
    Refactored provision.sh to take a singular
    that controls which service(s) are provisioned.

--- provision.sh
+++ provision.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# This script will provision all of the services. Each service will be setup in the following manner:
-#
+# This script will provision the service specified in the first argument,
+# or all services if 'all' is passed as the argument.
+# 
+# Service(s) will generally be setup in the following manner
+# (but refer to  individual ./provision-{service} scripts to be sure):
 # 1. Migrations run,
 # 2. Tenants—as in multi-tenancy—setup,
 # 3. Service users and OAuth clients setup in LMS,
 # 4. Static assets compiled/collected.
-
 
 set -e
 set -o pipefail
@@ -16,6 +18,30 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
+
+if [[ $# -ne 1 ]]; then
+	echo -e "${RED}Exactly one argument required: Name of service or 'all'.\n\
+	Example 1: ./provision.sh discovery\n\
+	Example 2: ./provision.sh all${NC}"
+	exit 1
+fi
+
+service=$1
+case $service in
+	all)
+		echo -e "${GREEN}Will provision all services.${NC}"
+		;;
+	lms|ecommerce|discovery|credentials|e2e|forum|notes|registrar)
+		echo -e "${GREEN}Will provision one service: ${service}.${NC}"
+		;;
+	studio)
+		echo -e "${YELLOW}Studio is provisioned along with LMS; try running './provision.sh lms'${NC}"
+		exit 0
+		;;
+	*)
+		echo -e "${YELLOW}Service '${service}' either doesn't exist or isn't provisionable. Exiting.${NC}"
+		exit 1
+esac		
 
 # Bring the databases online.
 docker-compose up -d mysql mongo
@@ -29,36 +55,38 @@ do
 done
 
 # In the event of a fresh MySQL container, wait a few seconds for the server to restart
-# This can be removed once https://github.com/docker-library/mysql/issues/245 is resolved.
+# See https://github.com/docker-library/mysql/issues/245 for why this is necessary.
 sleep 20
-
 echo -e "MySQL ready"
 
-# Ensure the MongoDB server is online and usable
-echo "Waiting for MongoDB"
-until docker exec -i edx.devstack.mongo mongo --eval "printjson(db.serverStatus())" &> /dev/null
-do
-  printf "."
-  sleep 1
-done
-
-echo -e "MongoDB ready"
-
-echo -e "${GREEN}Creating databases and users...${NC}"
+# Ensure that the MySQL databases and users are created for all IDAs.
+# (A no-op for databases and users that already exist).
+echo -e "${GREEN}Ensuring MySQL databases and users exist...${NC}"
 docker exec -i edx.devstack.mysql mysql -uroot mysql < provision.sql
-docker exec -i edx.devstack.mongo mongo < mongo-provision.js
 
-./provision-lms.sh
+# If necessary, ensure the MongoDB server is online and usable
+# and create its users.
+case $service in
+	"lms"|"studio"|"forum"|"")
+		echo "Waiting for MongoDB"
+		until docker exec -i edx.devstack.mongo mongo --eval "printjson(db.serverStatus())" &> /dev/null
+		do
+		  printf "."
+		  sleep 1
+		done
+		echo -e "MongoDB ready"
+		echo -e "${GREEN}Creating MongoDB users...${NC}"
+		docker exec -i edx.devstack.mongo mongo < mongo-provision.js
+		;;
+	*)
+		echo -e "${GREEN}MongoDB preparation not required; skipping.${NC}"
+esac
 
-# Nothing special needed for studio
-docker-compose $DOCKER_COMPOSE_FILES up -d studio
-./provision-ecommerce.sh
-./provision-discovery.sh
-./provision-credentials.sh
-./provision-e2e.sh
-./provision-forum.sh
-./provision-notes.sh
-./provision-registrar.sh
+# Run the service-specific provisioning script(s)
+for serv in ${service:-lms ecommerce discovery credentials e2e forum notes registrar}; do
+	echo -e "${GREEN} Provisioning ${serv}...${NC}"
+	./provision-${serv}.sh
+done
 
 docker image prune -f
 
