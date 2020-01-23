b3d7bb285206cda479b202abdc1fbed77e8b6ab0
sysadmintma <sysadmin@themoocagency.com>
2020-01-23 19:20:06 +0100

    Add make as prerequisite package
    In case it is not there...

--- install_devstack_p1.sh
+++ install_devstack_p1.sh
@@ -9,7 +9,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 echo "# Install docker and docker compose"
-sudo apt install curl tmux nano apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+sudo apt install curl tmux nano apt-transport-https ca-certificates curl gnupg-agent software-properties-common make -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update
