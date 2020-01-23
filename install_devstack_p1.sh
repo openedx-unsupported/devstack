30f66a5712f99cce29fc14686c417be450319420
aureliencroq <aurelien.croq@themoocagency.com>
2020-01-23 19:24:44 +0100

    Add docker compose in a specific version

--- install_devstack_p1.sh
+++ install_devstack_p1.sh
@@ -14,6 +14,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+sudo mv ./docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 echo "# Add your user to the docker group"
 sudo usermod -aG docker ${USER}
