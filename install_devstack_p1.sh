71d09e55ee10c73498b2f032dc35c7ca5a8d7ffd
sysadmintma <sysadmin@themoocagency.com>
2020-01-23 20:07:40 +0100

    auto yes

--- install_devstack_p1.sh
+++ install_devstack_p1.sh
@@ -12,8 +12,8 @@ echo "# Install docker and docker compose"
 sudo apt install curl tmux nano apt-transport-https ca-certificates curl gnupg-agent software-properties-common make -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > ./docker-compose
 sudo mv ./docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
