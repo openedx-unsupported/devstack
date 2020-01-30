712ec60601bdfa3fc5b7de33ed28601490ab2331
Belkacem <belkacem.bougueham@themoocagency.com>
2020-01-30 16:58:18 +0100

    Updating install file for atp_theme

--- install_devstack_p2.sh
+++ install_devstack_p2.sh
@@ -23,4 +23,32 @@ export OPENEDX_RELEASE=juniper.alpha1
 make dev.checkout
 make dev.clone
 make dev.provision
+
+echo '#Getting atp_juniper repo'
+cd ~/edx/devstack_files/devstack
+git checkout atp_juniper
+
+echo '#setting media folder and atp_theme'
+cd ~/edx/devstack_files/
+mkdir custom_theme
+cd custom_theme
+mkdir atp_theme
+cd atp_theme
+git init
+git remote add origin https://github.com/TheMOOCAgency/atp_theme.git
+git checkout atp_juniper
+git pull origin atp_juniper
+
+
+echo '#Setting env for theming'
+cd ~/edx/devstack_files/devstack/
+docker cp cms.yml edx.devstack.studio:/edx/etc/ &&
+docker cp lms.yml edx.devstack.studio:/edx/etc/ &&
+docker cp studio.yml edx.devstack.studio:/edx/etc/ &&
+docker cp cms.yml edx.devstack.lms:/edx/etc/ &&
+docker cp lms.yml edx.devstack.lms:/edx/etc/
+
+echo '#Lauching devstack'
+make lms-restart
+make studio-restart
 make dev.up
