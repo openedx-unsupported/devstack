70c890b25cef4a334fb10ba167e4bd3f476b5dbf
Kyle McCormick <kmccormick@edx.org>
2020-02-27 12:12:05 -0500

    Clean up provision-ida.sh

--- provision-ida.sh
+++ provision-ida.sh
@@ -10,18 +10,27 @@ container_name=${4:-$1} # (Optional) The name of the container.  If missing, wil
 
 docker-compose $DOCKER_COMPOSE_FILES up -d $app_name
 
+run_in_container_venv() {
+	bash_command="cd /edx/app/${app_name}/${app_name} && source ../${app_name}_env && $@"
+	docker exec -t edx.devstack."$container_name" bash -c \""$bash_command"\"
+}
+
 echo -e "${GREEN}Installing requirements for ${app_name}...${NC}"
-docker exec -t edx.devstack.${container_name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make requirements' -- "$app_name"
+run_in_container_venv make requirements
 
 echo -e "${GREEN}Running migrations for ${app_name}...${NC}"
-docker exec -t edx.devstack.${container_name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make migrate' -- "$app_name"
+run_in_container_venv make migrate
 
 echo -e "${GREEN}Creating super-user for ${app_name}...${NC}"
-docker exec -t edx.devstack.${container_name}  bash -c 'source /edx/app/$1/$1_env && echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None" | python /edx/app/$1/$1/manage.py shell' -- "$app_name"
+run_in_container_venv ./manage.py shell -c \
+"from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.create_superuser('edx', 'edx@example.com', 'edx') \
+if not User.objects.filter(username='edx').exists() else None"
 
 ./provision-ida-user.sh $app_name $client_name $client_port
 
 # Compile static assets last since they are absolutely necessary for all services. This will allow developers to get
 # started if they do not care about static assets
 echo -e "${GREEN}Compiling static assets for ${app_name}...${NC}"
-docker exec -t edx.devstack.${container_name}  bash -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && make static' -- "$app_name"
+run_in_container_venv make static
