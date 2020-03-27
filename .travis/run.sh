ee8bcc43598b7e20441e62892d9181c62d62b25b
Kyle McCormick <kmccormick@edx.org>
2020-03-27 15:51:01 -0400

    Simplify .travis/run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 set -x
 
 if [[ "$DEVSTACK" == "analytics_pipeline" ]]; then
@@ -10,17 +11,9 @@ if [[ "$DEVSTACK" == "analytics_pipeline" ]]; then
     make analytics-pipeline-devstack-test
 elif [[ "$DEVSTACK" == "lms+marketing" ]]; then
     make dev.provision.services.lms
-    make dev.up.lms  # Brings up Discovery and Forum as dependencies.
-    sleep 60  # LMS needs like 60 seconds to come up.
     make validate-lms-volume
     make up-marketing-detached
 else
-    case "$DEVSTACK" in
-        # Other services can be added in here seperated by '|', i.e. "registrar|discovery)"
-        registrar)
-            echo "Provisioning LMS first because $DEVSTACK requires it"
-            make dev.provision.services.lms
-    esac
     make dev.provision.services."$DEVSTACK"
     make dev.up."$DEVSTACK"
     sleep 60
