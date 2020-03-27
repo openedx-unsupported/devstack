dfb12c10bb3ef898acee30700d0a4e1b6b317669
Kyle McCormick <kmccormick@edx.org>
2020-03-27 10:41:28 -0400

    Simplify handling of Analytics Pipeline and Marketing
    * Allow analytics pipeline to be provisioned and run
      just like other services.
    * Test marketing site in its own shard.
    * Allow marketing site to be provisioned and run just
      like other services.
    * Clean up redundant Makefile rules a bit.
    * Fix quality issues in provision.sh with
      shellcheck==0.7.0.

--- check.sh
+++ check.sh
@@ -105,6 +105,12 @@ if should_check credentials; then
         "curl --fail -L http://localhost:18150/health"
 fi
 
+if should_check marketing; then
+    echo "Seeing if we can curl root of Marketing site: "
+    run_check marketing_curl marketing \
+        "curl http://localhost:8080"
+fi
+
 echo "Successful checks:${succeeded:- NONE}"
 echo "Failed checks:${failed:- NONE}"
 if [[ "$succeeded" ]]; then
---
+++ provision-marketing.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -x
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+docker-compose $DOCKER_COMPOSE_FILES up -d marketing
+
+set +x
+echo -e "${YELLOW}edX Marketing Site is not fully provisioned yet.${NC}"
+echo -e "${YELLOW}For full setup, see:${NC}"
+echo -e "${YELLOW}    https://openedx.atlassian.net/wiki/spaces/ENG/pages/159162183/Marketing+Site${NC}"
+set -x
