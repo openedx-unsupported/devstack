b8e49cca156f756f555ebffe8179d55ea3c2e227
Kyle McCormick <kmccormick@edx.org>
2020-04-20 11:26:50 -0400

    Set configuration options with '?=', not '='
    That way, they can be overriden on the command line.

--- options.mk
+++ options.mk
@@ -1,8 +1,8 @@
 # DEFAULT DEVSTACK OPTIONS
 # Included into Makefile and exported to command environment.
 # Defaults are listed in this file.
-# For configuring your local Devstack, create a file called
-# 'options.local.mk' and add your preferred overrides.
+# Local git-ignored overrides can be configured by creating `options.local.mk`.
+# Variables are set here with ?= to allow for overriding them on the command line.
 
 # Folder in which we looks for repositories.
 # Defaults to parent.
@@ -11,13 +11,13 @@ DEVSTACK_WORKSPACE ?= $(shell pwd)/..
 # Name of Docker Compose project.
 # See https://docs.docker.com/compose/reference/envvars/#compose_project_name
 # Defaults to 'devstack'.
-COMPOSE_PROJECT_NAME=devstack
+COMPOSE_PROJECT_NAME ?= devstack
 
 # Whether we should always copy programs to LMS cache upon LMS startup.
 # If 'true', then run `make dev.cache-programs` whenever we bring up
 # containers.
 # Defaults to false.
-ALWAYS_CACHE_PROGRAMS=false
+ALWAYS_CACHE_PROGRAMS ?= false
 
 # Load local overrides to options.
 # The dash in front of 'include' makes it so we don't
