cd4eb707698be0fa0f1e1de716fe6b7b25524ceb
Simon Chen <schen@edx.org>
2020-08-05 09:29:17 -0400

    MST-306 Add course-authoring MFE to the devstack (#593)

--- repo.sh
+++ repo.sh
@@ -35,6 +35,7 @@ repos=(
 )
 
 non_release_repos=(
+    "https://github.com/edx/frontend-app-course-authoring.git"
     "https://github.com/edx/frontend-app-learning.git"
     "https://github.com/edx/registrar.git"
     "https://github.com/edx/frontend-app-program-console.git"
@@ -55,6 +56,7 @@ ssh_repos=(
 )
 
 non_release_ssh_repos=(
+    "git@github.com:edx/frontend-app-course-authoring.git"
     "git@github.com:edx/frontend-app-learning.git"
     "git@github.com:edx/registrar.git"
     "git@github.com:edx/frontend-app-program-console.git"
