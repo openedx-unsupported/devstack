814393350c0bdf273a25989b6be0a3f783aeebda
jinder1s <msingh@edx.org>
2021-03-22 10:23:38 -0400

    sqash: small fixes

--- load_test_data.py
+++ load_test_data.py
@@ -7,22 +7,21 @@ import subprocess
 
 def main(input_yaml_path):
     with open(input_yaml_path, 'r') as f:
-        paths = yaml.safe_load(f)
-        for data_spec_path in paths:
+        top_data_spec_yaml = yaml.safe_load(f)
+        for data_spec_path in top_data_spec_yaml:
             ida_name = data_spec_path['ida_name']
             ida_data_spec_yaml = data_spec_path['data_spec_path']
             print(f"{ida_name}: {ida_data_spec_yaml} ")
             if ida_name == "lms" or ida_name == "cms":
                 print("running suprocess command")
-                # TODO:Debug(jinder): this command does not run with error: 'manage.py' no such file or directory.
+                # FYI, this does not work. Issues with not finding paths
                 subprocess.run(f"docker-compose exec -T {ida_name} bash -c 'python manage.py' {ida_name} load_test_data --data-file-path {ida_data_spec_yaml}", shell=True)
             else:
-                pass
-                # subprocess.call(f"docker-compose exec -T {ida_name} bash -c 'python manage.py' load_test_data --data-file-path {ida_data_spec_yaml} ")
+                subprocess.call(f"docker-compose exec -T {ida_name} bash -c 'python manage.py' load_test_data --data-file-path {ida_data_spec_yaml} ")
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print("did not receive enough inputs")
+        print("Path to data spec yaml not specified")
