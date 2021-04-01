f3f0bd70928c72238666c61b73dad9fe94cd40bd
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2021-04-01 17:00:23 -0400

    feat: add "Edit on Github" button in breadcrumbs (#713)
    * feat: add "Edit on Github" button in breadcrumbs
    
    Currently, viewers of documentation would need to know alot about a page
    to know where they need to go to fix outdated docs. "Edit on Github"
    button should decrease the barrier in entry for improving docs.
    
    * chore:  running upgrade to get new edx-spinx-theme

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,15 @@ author = edx_theme.AUTHOR
 project_title = 'devstack'
 documentation_title = "{project_title}".format(project_title=project_title)
 
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "edx", # Username
+    "github_repo": "devstack", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
