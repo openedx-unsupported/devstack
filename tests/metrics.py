f6e978182531f111f590c6b725bdd523efbb2923
Tim McCormack <tmccormack@edx.org>
2021-05-07 20:59:26 +0000

    test: Convert Expect tests to pytest + pexpect
    This allows us to keep the tests in our usual language rather than adding
    a new one.

---
+++ tests/metrics.py
@@ -0,0 +1,43 @@
+import json
+import os
+import pexpect
+import pytest
+
+
+def test_metrics():
+    """
+    Test that dev.up.% is instrumented for metrics collection.
+    """
+
+    # Some setup and preconditions first...
+
+    assert os.environ.get('DEVSTACK_METRICS_TESTING'), \
+        "You need a DEVSTACK_METRICS_TESTING=debug if running this test " \
+        "locally since this environment variable both enables printing of " \
+        "metrics and also marks sent metric events as test data."
+        
+    config_dir = os.path.expanduser('~/.config/devstack')
+    config_path = os.path.join(config_dir, 'metrics.json')
+
+    assert not os.path.isfile(config_path), \
+        "You already have a config file; failing now to avoid overwriting it."
+
+    # Set up a fake Segment write key so that CI tests are "opted in" and
+    # will collect metrics.
+    os.makedirs(config_dir, exist_ok=True)
+    with open(config_path, 'w') as f:
+        f.write(json.dumps({"segment_write_key": "fake"}))
+
+    # OK, the actual test:
+
+    try:
+        p = pexpect.spawn('make dev.up.redis', timeout=60)
+        p.expect(r'Send metrics info:')
+        p.expect(r'dev\.up\.redis')
+    finally:
+        # Clean up before exiting.
+        with open(config_path, 'r') as f:
+            # For debugging...
+            print("Metrics config file in effect was: " + f.read())
+
+        os.remove(config_path)
