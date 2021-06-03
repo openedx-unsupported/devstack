openedx-unsupported/devstack

commit d1edbe7573aa56d39408ae2d41c46f25b5893443

feat: adding metrics collections to more targets (#758)
* feat: adding metrics collections to more targets

particularly:
dev.clone...
dev.provision...
dev.pull...
dev.up.%

Makefile
@@ -58,7 +58,9 @@
         dev.sync.requirements dev.sync.up dev.up dev.up.attach dev.up.shell \
         dev.up.without-deps dev.up.without-deps.shell dev.up.with-programs \
         dev.up.with-watchers dev.validate docs e2e-tests e2e-tests.with-shell \
-        help requirements impl-dev.pull selfcheck upgrade upgrade \
+        help requirements impl-dev.clone.https impl-dev.clone.ssh impl-dev.provision \
+        impl-dev.pull impl-dev.pull.without-deps impl-dev.up impl-dev.up.attach \
+        impl-dev.up.without-deps selfcheck upgrade upgrade \
         validate-lms-volume vnc-passwords
 
 # Load up options (configurable through options.local.mk).
@@ -187,12 +189,17 @@ dev.checkout: ## Check out "openedx-release/$OPENEDX_RELEASE" in each repo if se
 
 dev.clone: dev.clone.ssh ## Clone service repos to the parent directory.
 
-dev.clone.https: ## Clone service repos using HTTPS method to the parent directory.
+impl-dev.clone.https: ## Clone service repos using HTTPS method to the parent directory.
 	./repo.sh clone
 
-dev.clone.ssh: ## Clone service repos using SSH method to the parent directory.
+dev.clone.https: ## Clone service repos using HTTPS method to the parent directory.
+	@scripts/send-metrics.py wrap "$@"
+
+impl-dev.clone.ssh: ## Clone service repos using SSH method to the parent directory.
 	./repo.sh clone_ssh
 
+dev.clone.ssh: ## Clone service repos using SSH method to the parent directory.
+	@scripts/send-metrics.py wrap "$@"
 
 ########################################################################################
 # Developer interface: Docker image management.
@@ -201,12 +208,15 @@ dev.clone.ssh: ## Clone service repos using SSH method to the parent directory.
 dev.pull.without-deps: _expects-service-list.dev.pull.without-deps
 
 dev.pull.without-deps.%: ## Pull latest Docker images for specific services.
+	@scripts/send-metrics.py wrap "dev.pull.without-deps.$*"
+
+impl-dev.pull.without-deps.%: ## Pull latest Docker images for specific services.
 	docker-compose pull $$(echo $* | tr + " ")
 
 dev.pull:
 	@scripts/send-metrics.py wrap "$@"
 
-impl-dev.pull: ##
+impl-dev.pull:
 	@scripts/make_warn_default_large.sh "dev.pull"
 
 dev.pull.large-and-slow: dev.pull.$(DEFAULT_SERVICES) ## Pull latest Docker images required by default services.
@@ -214,23 +224,33 @@ dev.pull.large-and-slow: dev.pull.$(DEFAULT_SERVICES) ## Pull latest Docker imag
 
 # Wildcards must be below anything they could match
 dev.pull.%: ## Pull latest Docker images for services and their dependencies.
+	@scripts/send-metrics.py wrap "dev.pull.$*"
+
+impl-dev.pull.%: ## Pull latest Docker images for services and their dependencies.
 	docker-compose pull --include-deps $$(echo $* | tr + " ")
 
 ########################################################################################
 # Developer interface: Database management.
 ########################################################################################
 
-dev.provision: dev.check-memory ## Provision dev environment with default services, and then stop them.
+impl-dev.provision: ## Provision dev environment with default services, and then stop them.
 	# We provision all default services as well as 'e2e' (end-to-end tests).
 	# e2e is not part of `DEFAULT_SERVICES` because it isn't a service;
 	# it's just a way to tell ./provision.sh that the fake data for end-to-end
 	# tests should be prepared.
+	make dev.check-memory
 	$(WINPTY) bash ./provision.sh $(DEFAULT_SERVICES)+e2e
 	make dev.stop
 
-dev.provision.%: ## Provision specified services.
+dev.provision: ## Provision dev environment with default services, and then stop them.
+	@scripts/send-metrics.py wrap "$@"
+
+impl-dev.provision.%: dev.check-memory ## Provision specified services.
 	echo $*
 	$(WINPTY) bash ./provision.sh $*
+
+dev.provision.%: ## Provision specified services.
+	@scripts/send-metrics.py wrap "dev.provision.$*"
 
 dev.backup: dev.up.mysql+mysql57+mongo+elasticsearch+elasticsearch7 ## Write all data volumes to the host.
 	docker run --rm --volumes-from $$(make -s dev.print-container.mysql) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mysql.tar.gz /var/lib/mysql
@@ -278,8 +298,11 @@ dev.drop-db.%: ## Irreversably drop the contents of a MySQL database in each mys
 
 dev.up.attach: _expects-service.dev.up.attach
 
-dev.up.attach.%: ## Bring up a service and its dependencies + and attach to it.
+impl-dev.up.attach.%: ## Bring up a service and its dependencies + and attach to it.
 	docker-compose up $*
+
+dev.up.attach.%: ## Bring up a service and its dependencies + and attach to it.
+	@scripts/send-metrics.py wrap "dev.up.attach.$*"
 
 dev.up.shell: _expects-service.dev.up.shell
 
@@ -299,8 +322,11 @@ dev.up.with-watchers.%: ## Bring up services and their dependencies + asset watc
 
 dev.up.without-deps: _expects-service-list.dev.up.without-deps
 
-dev.up.without-deps.%: dev.check-memory ## Bring up services by themselves.
+impl-dev.up.without-deps.%: dev.check-memory ## Bring up services by themselves.
 	docker-compose up --d --no-deps $$(echo $* | tr + " ")
+
+dev.up.without-deps.%: ## Bring up services by themselves.
+	@scripts/send-metrics.py wrap "dev.up.without-deps.$*"
 
 dev.up.without-deps.shell: _expects-service.dev.up.without-deps.shell
 

commit 6ba733fdbbfa556f9b0cbe108262cd9a5b24f146

fix: Make `make` quieter when printing container IDs (unbreak provisioning)
Provisioning was broken by commit d1edbe7573a/PR #758 because it introduced
a second layer of make, which caused the now-inner make call to target
`dev.print-container.%` to print debugging info that looked like
`make[2]: Entering directory '/home/timmc/edx-repos/devstack'`.

Calls to `dev.print-container.%` already used the `--silent` flag, which is
correct to use here but is insufficient because it doesn't suppress
printing of the current directory when a recursive make call is made.

This commit adds `--no-print-directory` to all calls to that target as well
as to the documented way to call the target.

It is not clear why this was not caught by automated tests; although they
run `dev.provision.%` rather than the broader `dev.provision`, the effect
should have been the same.

Makefile
@@ -253,18 +253,18 @@ dev.provision.%: ## Provision specified services.
 	@scripts/send-metrics.py wrap "dev.provision.$*"
 
 dev.backup: dev.up.mysql+mysql57+mongo+elasticsearch+elasticsearch7 ## Write all data volumes to the host.
-	docker run --rm --volumes-from $$(make -s dev.print-container.mysql) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mysql.tar.gz /var/lib/mysql
-	docker run --rm --volumes-from $$(make -s dev.print-container.mysql57) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mysql57.tar.gz /var/lib/mysql
-	docker run --rm --volumes-from $$(make -s dev.print-container.mongo) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mongo.tar.gz /data/db
-	docker run --rm --volumes-from $$(make -s dev.print-container.elasticsearch) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/elasticsearch.tar.gz /usr/share/elasticsearch/data
-	docker run --rm --volumes-from $$(make -s dev.print-container.elasticsearch7) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/elasticsearch7.tar.gz /usr/share/elasticsearch/data
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mysql) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mysql.tar.gz /var/lib/mysql
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mysql57) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mysql57.tar.gz /var/lib/mysql
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mongo) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/mongo.tar.gz /data/db
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.elasticsearch) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/elasticsearch.tar.gz /usr/share/elasticsearch/data
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.elasticsearch7) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zcvf /backup/elasticsearch7.tar.gz /usr/share/elasticsearch/data
 
 dev.restore: dev.up.mysql+mysql57+mongo+elasticsearch+elasticsearch7 ## Restore all data volumes from the host. WILL OVERWRITE ALL EXISTING DATA!
-	docker run --rm --volumes-from $$(make -s dev.print-container.mysql) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mysql.tar.gz
-	docker run --rm --volumes-from $$(make -s dev.print-container.mysql57) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mysql57.tar.gz
-	docker run --rm --volumes-from $$(make -s dev.print-container.mongo) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mongo.tar.gz
-	docker run --rm --volumes-from $$(make -s dev.print-container.elasticsearch) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/elasticsearch.tar.gz
-	docker run --rm --volumes-from $$(make -s dev.print-container.elasticsearch7) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/elasticsearch7.tar.gz
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mysql) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mysql.tar.gz
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mysql57) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mysql57.tar.gz
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.mongo) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/mongo.tar.gz
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.elasticsearch) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/elasticsearch.tar.gz
+	docker run --rm --volumes-from $$(make --silent --no-print-directory dev.print-container.elasticsearch7) -v $$(pwd)/.dev/backups:/backup debian:jessie tar zxvf /backup/elasticsearch7.tar.gz
 
 # List of Makefile targets to run database migrations, in the form dev.migrate.$(service)
 # Services will only have their migrations added here
@@ -354,7 +354,7 @@ dev.ps: ## View list of created services and their statuses.
 	docker-compose ps
 
 dev.print-container.%: ## Get the ID of the running container for a given service.
-	@# Can be run as ``make --silent dev.print-container.$service`` for just ID.
+	@# Can be run as ``make --silent --no-print-directory dev.print-container.$service`` for just ID.
 	@echo $$(docker-compose ps --quiet $*)
 
 dev.restart-container: ## Restart all service containers.
@@ -432,7 +432,7 @@ dev.logs.%: ## View the logs of the specified service container.
 dev.attach: _expects-service.dev.attach
 
 dev.attach.%: ## Attach to the specified service container process for debugging & seeing logs.
-	docker attach "$$(make --silent dev.print-container.$*)"
+	docker attach "$$(make --silent --no-print-directory dev.print-container.$*)"
 
 dev.shell: _expects-service.dev.shell
 

Makefile.edx
@@ -24,7 +24,7 @@ dev.provision.whitelabel:
 # AND the test must be setup using the 'dev.provision.whitelabel' target.
 whitelabel-tests:
 	docker run -d --name=devstack.whitelabel --network=${COMPOSE_PROJECT_NAME:-devstack}_default -v ${DEVSTACK_WORKSPACE}/edx-e2e-tests:/edx-e2e-tests -v ${DEVSTACK_WORKSPACE}/edx-platform:/edx-e2e-tests/lib/edx-platform --env-file ${DEVSTACK_WORKSPACE}/edx-e2e-tests/devstack_env edxops/e2e
-	docker cp ${DEVSTACK_WORKSPACE}/edx-themes/edx-platform/run_whitelabel_tests.sh $(make --silent dev.print-container.devstack.whitelabel)":/tmp/run_whitelabel_tests.sh
+	docker cp ${DEVSTACK_WORKSPACE}/edx-themes/edx-platform/run_whitelabel_tests.sh $(make --silent --no-print-directory dev.print-container.devstack.whitelabel)":/tmp/run_whitelabel_tests.sh
 	docker exec -t devstack.whitelabel env TEST_ENV=devstack TERM=$(TERM) bash /tmp/run_whitelabel_tests.sh
 
 whitelabel-cleanup:

in
@@ -17,10 +17,10 @@ set -u
 service="$1"
 shift
 
-container_id=$(make --silent dev.print-container."$service")
+container_id=$(make --silent --no-print-directory dev.print-container."$service")
 if [[ -z "$container_id" ]]; then
-	make --silent dev.up."$service"
-	container_id=$(make --silent dev.print-container."$service")
+	make --silent --no-print-directory dev.up."$service"
+	container_id=$(make --silent --no-print-directory dev.print-container."$service")
 fi
 
 docker exec -it "$container_id" bash -c "$*"

load-db.sh
@@ -17,6 +17,6 @@ then
 fi
 
 echo "Loading the $1 database..."
-mysql_container=$(make -s dev.print-container.mysql57)
+mysql_container=$(make --silent --no-print-directory dev.print-container.mysql57)
 docker exec -i "$mysql_container" mysql -uroot $1 < $1.sql
 echo "Finished loading the $1 database!"

provision-e2e.sh
@@ -10,7 +10,7 @@ elif [ ! -d "$DEVSTACK_WORKSPACE" ]; then
 fi
 
 # Copy the test course tarball into the studio container
-docker cp ${DEVSTACK_WORKSPACE}/edx-e2e-tests/upload_files/course.tar.gz "$(make --silent dev.print-container.studio)":/tmp/
+docker cp ${DEVSTACK_WORKSPACE}/edx-e2e-tests/upload_files/course.tar.gz "$(make --silent --no-print-directory dev.print-container.studio)":/tmp/
 
 # Extract the test course tarball
 docker-compose exec -T studio bash -c 'cd /tmp && tar xzf course.tar.gz'

update-dbs-init-sql-scripts.sh
@@ -24,7 +24,7 @@ make dev.clone.ssh
 make dev.provision.services.lms+ecommerce
 
 # dump schema and data from mysql databases in the mysql docker container and copy them to current directory in docker host
-MYSQL_DOCKER_CONTAINER="$(make --silent dev.print-container.mysql57)"
+MYSQL_DOCKER_CONTAINER="$(make --silent --no-print-directory dev.print-container.mysql57)"
 for DB_NAME in "${DBS[@]}"; do
     DB_CREATION_SQL_SCRIPT="${DB_NAME}.sql"
     if [[ " ${EDXAPP_DBS[@]} " =~ " ${DB_NAME} " ]]; then

upgrade_mongo_4_0.sh
@@ -11,7 +11,7 @@ export MONGO_VERSION=3.4.24
 current_mongo_version="3.4"
 echo -e "${GREEN}Sarting Mongo ${MONGO_VERSION}${NC}"
 make dev.up.mongo
-mongo_container="$(make -s dev.print-container.mongo)"
+mongo_container="$(make --silent --no-print-directory dev.print-container.mongo)"
 
 echo -e "${GREEN}Waiting for MongoDB...${NC}"
 until docker exec "$mongo_container" mongo --eval 'db.serverStatus()' &> /dev/null
@@ -49,7 +49,7 @@ export MONGO_VERSION=3.6.17
 echo
 echo -e "${GREEN}Restarting Mongo on version ${MONGO_VERSION}${NC}"
 make dev.up.mongo
-mongo_container="$(make -s dev.print-container.mongo)"
+mongo_container="$(make --silent --no-print-directory dev.print-container.mongo)"
 
 echo -e "${GREEN}Waiting for MongoDB...${NC}"
 until docker exec "$mongo_container" mongo --eval 'db.serverStatus()' &> /dev/null
@@ -78,7 +78,7 @@ export MONGO_VERSION=4.0.22
 echo
 echo -e "${GREEN}Restarting Mongo on version ${MONGO_VERSION}${NC}"
 make dev.up.mongo
-mongo_container="$(make -s dev.print-container.mongo)"
+mongo_container="$(make --silent --no-print-directory dev.print-container.mongo)"
 
 echo -e "${GREEN}Waiting for MongoDB...${NC}"
 until docker exec "$mongo_container" mongo --eval 'db.serverStatus()' &> /dev/null