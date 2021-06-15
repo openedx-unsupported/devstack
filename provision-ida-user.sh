0a2508d333b6b0c677c29d07fc9897ab6bfdaaed
Tim McCormack <tmccormack@edx.org>
2021-06-15 15:19:41 +0000

    fix: Remove more error suppression: IDA user, credentials (#776)
    - The errors in provision-ida-user.sh were solved by commit 7b029fe9 in
      PR #772 -- these services needed the LMS to be provisioned as well.
    - One of these credentials error suppression comments may have been
      spurious, or at least copy-pasted but not adjusted properly.
    - The other does seem to have been fixed by adding another `credentials`
      path element.

--- provision-ida-user.sh
+++ provision-ida-user.sh
@@ -2,12 +2,6 @@
 set -eu -o pipefail
 set -x
 
-# FIXME[bash-e]: Bash scripts should use -e -- but this script fails
-# with the following errors for ecommerce & edx_notes_api:
-# - RuntimeError: Model class openedx.core.djangoapps.content_libraries.models.ContentLibrary doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS.
-# - django.db.utils.ProgrammingError: (1146, "Table 'edxapp.auth_user' doesn't exist")
-set +e
-
 #This script depends on the LMS being up!
 
 . scripts/colors.sh
@@ -24,10 +18,3 @@ docker-compose exec -T lms  bash -e -c 'source /edx/app/edxapp/edxapp_env && pyt
 # Create the DOT applications - one for single sign-on and one for backend service IDA-to-IDA authentication.
 docker-compose exec -T lms  bash -e -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris "http://localhost:$3/complete/edx-oauth2/" --client-id "$1-sso-key" --client-secret "$1-sso-secret" --scopes "user_id" $1-sso $1_worker' -- "$app_name" "$client_name" "$client_port"
 docker-compose exec -T lms  bash -e -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id "$1-backend-service-key" --client-secret "$1-backend-service-secret" $1-backend-service $1_worker' -- "$app_name" "$client_name" "$client_port"
-
-
-# FIXME[bash-e]: Suppress last error so that calling script can set -e
-# for itself. Remove this once *this* script is run entirely with `-e`
-# in effect (or at least the last command is no longer erroring for
-# any services).
-true
