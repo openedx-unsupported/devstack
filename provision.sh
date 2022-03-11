bbc89c831ec59ee767e49f7f7d53507cf67f5074
Rebecca Graber <rgraber@edx.org>
2022-03-11 14:37:09 -0500

    feat: up time

--- provision.sh
+++ provision.sh
@@ -138,7 +138,7 @@ done
 
 # In the event of a fresh MySQL container, wait a few seconds for the server to restart
 # See https://github.com/docker-library/mysql/issues/245 for why this is necessary.
-sleep 2
+sleep 5
 
 echo "${GREEN}Waiting for MySQL 5.7.${NC}"
 until docker-compose exec -T mysql57 bash -e -c "mysql -uroot -se \"SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'root')\"" &> /dev/null
