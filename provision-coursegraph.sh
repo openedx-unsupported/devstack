464e38de54190c8285da7d2782d42102b78716c3
Kyle McCormick <kyle@tcril.org>
2022-03-25 12:37:03 -0400

    fix: coursegraph dump should come from cms, not lms (#899)

--- provision-coursegraph.sh
+++ provision-coursegraph.sh
@@ -12,11 +12,11 @@ echo -e "${GREEN}   Ensuring Coursegraph image is up to date...${NC}"
 docker-compose rm --force --stop coursegraph
 docker-compose pull coursegraph
 
-echo -e "${GREEN}   Starting Coursegraph and LMS...${NC}"
-docker-compose up -d coursegraph lms
+echo -e "${GREEN}   Starting Coursegraph and CMS...${NC}"
+docker-compose up -d coursegraph studio
 sleep 10  # Give Neo4j some time to boot up.
 
-echo -e "${GREEN}   Updating LMS courses in Coursegraph...${NC}"
-docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && ./manage.py lms dump_to_neo4j --host coursegraph.devstack.edx --user neo4j --password edx'
+echo -e "${GREEN}   Updating CMS courses in Coursegraph...${NC}"
+docker-compose exec studio bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && ./manage.py cms dump_to_neo4j --host coursegraph.devstack.edx --user neo4j --password edx'
 
-echo -e "${GREEN}   Coursegraph is now up-to-date with LMS!${NC}"
+echo -e "${GREEN}   Coursegraph is now up-to-date with CMS!${NC}"
