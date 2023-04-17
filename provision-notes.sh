3d636c66de108b4478c9c3d8d1972105d30e6579
Soban Javed <iamsobanjaved@gmail.com>
2023-04-17 14:52:13 +0500

    feat: introduce annsible-free docker image for edx-notes-apo

--- provision-notes.sh
+++ provision-notes.sh
@@ -6,8 +6,30 @@ set -eu -o pipefail
 set -x
 
 # Common provisioning tasks for IDAs, including requirements, migrations, oauth client creation, etc.
-./provision-ida.sh edx_notes_api edx-notes 18120 edx_notes_api
+# ./provision-ida.sh edx_notes_api edx-notes 18120 edx_notes_api
+
+name=edx_notes_api
+port=18734
+client_name=edx-notes  # The name of the Oauth client stored in the edxapp DB.
+
+docker-compose up -d $name
+
+echo -e "${GREEN}Installing requirements for ${name}...${NC}"
+docker-compose exec -T ${name}  bash -e -c 'cd /edx/app/notes && make requirements' -- "$name"
+
+echo -e "${GREEN}Running migrations for ${name}...${NC}"
+docker-compose exec -T ${name}  bash -e -c 'cd /edx/app/notes && make migrate' -- "$name"
+
+echo -e "${GREEN}Creating super-user for ${name}...${NC}"
+docker-compose exec -T ${name}  bash -e -c 'cho "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None" | python /edx/app/notes/manage.py shell' -- "$name"
+
+./provision-ida-user.sh $name $client_name $port
+
+# Compile static assets last since they are absolutely necessary for all services. This will allow developers to get
+# started if they do not care about static assets
+echo -e "${GREEN}Compiling static assets for ${name}...${NC}"
+docker-compose exec -T ${name}  bash -e -c 'cd /edx/app/notes && make static' -- "$name"
 
 # This will build the elasticsearch index for notes.
-echo -e "${GREEN}Creating indexes for edx_notes_api...${NC}"
-docker-compose exec -T edx_notes_api bash -e -c 'source /edx/app/$1/$1_env && cd /edx/app/$1/$1/ && python manage.py search_index --rebuild -f' -- edx_notes_api
+echo -e "${GREEN}Creating indexes for ${name}...${NC}"
+docker-compose exec -T ${name} bash -e -c 'cd /edx/app/notes/ && python manage.py search_index --rebuild -f'
