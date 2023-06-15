2803178b40c31cc9466f421a4745a5dbf92dde57
Tim McCormack <tmccormack@edx.org>
2023-06-15 15:55:53 -0400

    feat: Update GH org for moved frontend app repo (#1100)

--- repo.sh
+++ repo.sh
@@ -45,7 +45,7 @@ non_release_repos=(
     "https://github.com/openedx/frontend-app-program-console.git"
     "https://github.com/openedx/frontend-app-account.git"
     "https://github.com/openedx/frontend-app-profile.git"
-    "https://github.com/edx/frontend-app-ora-grading.git"
+    "https://github.com/openedx/frontend-app-ora-grading.git"
 )
 
 ssh_repos=(
@@ -73,7 +73,7 @@ non_release_ssh_repos=(
     "git@github.com:openedx/frontend-app-program-console.git"
     "git@github.com:openedx/frontend-app-account.git"
     "git@github.com:openedx/frontend-app-profile.git"
-    "git@github.com:edx/frontend-app-ora-grading.git"
+    "git@github.com:openedx/frontend-app-ora-grading.git"
 )
 
 if [ -n "${OPENEDX_RELEASE}" ]; then
