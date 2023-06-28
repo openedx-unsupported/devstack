6b982d9da3543cb0f2676d24f365009647b48266
Tim McCormack <tmccormack@edx.org>
2023-06-28 15:51:32 -0400

    fix: Only create users in mysql 8.0 if they don't already exist (#1112)
    This issue was breaking provisioning early in the process.
    
    The volumes for mysql57 and mysql80 may still contain users from previous
    provisioning runs. The old provisioning script for mysql 5.7 (provision.sql)
    uses implicit user creation via the `GRANT` statement, which is no longer
    supported in mysql 8.0. Therefore, the `CREATE USER` statements in
    provision-mysql80.sql fail. The fix here is to change to `CREATE USER IF
    NOT EXISTS`.
    
    I've also deleted `drop-mysql-user.sql`. I suspect it was only used during
    testing to enable repeated tests of provisioning, but it's not referenced
    anywhere in the PR that adds it (https://github.com/openedx/devstack/pull/1097).

--- provision-mysql80.sql
+++ provision-mysql80.sql
@@ -1,29 +1,33 @@
+-- The use of `CREATE USER IF NOT EXISTS` is necessary since the
+-- mysql80_data volume may already contain these users due to previous
+-- provisioning https://github.com/openedx/devstack/issues/1113
+
 CREATE DATABASE IF NOT EXISTS credentials;
-CREATE USER 'credentials001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'credentials001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON credentials.* TO 'credentials001'@'%';
 
 CREATE DATABASE IF NOT EXISTS discovery;
-CREATE USER 'discov001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'discov001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON discovery.* TO 'discov001'@'%';
 
 CREATE DATABASE IF NOT EXISTS ecommerce;
-CREATE USER 'ecomm001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'ecomm001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON ecommerce.* TO 'ecomm001'@'%';
 
 CREATE DATABASE IF NOT EXISTS notes;
-CREATE USER 'notes001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'notes001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON notes.* TO 'notes001'@'%';
 
 CREATE DATABASE IF NOT EXISTS registrar;
-CREATE USER 'registrar001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'registrar001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON registrar.* TO 'registrar001'@'%';
 
 CREATE DATABASE IF NOT EXISTS xqueue;
-CREATE USER 'xqueue001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'xqueue001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON xqueue.* TO 'xqueue001'@'%';
 
 CREATE DATABASE IF NOT EXISTS `dashboard`;
-CREATE USER 'analytics001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'analytics001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON `dashboard`.* TO 'analytics001'@'%';
 
 CREATE DATABASE IF NOT EXISTS `analytics-api`;
@@ -37,7 +41,7 @@ GRANT ALL ON `reports_v1`.* TO 'analytics001'@'%';
 
 CREATE DATABASE IF NOT EXISTS edxapp;
 CREATE DATABASE IF NOT EXISTS edxapp_csmh;
-CREATE USER 'edxapp001'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'edxapp001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON edxapp.* TO 'edxapp001'@'%';
 GRANT ALL ON edxapp_csmh.* TO 'edxapp001'@'%';
 
