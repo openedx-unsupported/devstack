676d3333358612d87ba98ed9f47084c25668dd5a
Rebecca Graber <rgraber@edx.org>
2023-09-14 13:49:14 -0400

    feat: remove docker-compose from devstack (#1172)

--- destroy.sh
+++ destroy.sh
@@ -4,5 +4,5 @@ set -eu -o pipefail
 read -p "This will delete all data in your devstack. Would you like to proceed? [y/n] " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    docker-compose down -v
+    docker compose down -v
 fi
